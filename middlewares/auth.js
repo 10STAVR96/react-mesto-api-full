e991e95988099960a33004c7cefd656c0f2d2d2a
Vitaliy Maksimov <vetal199624@mail.ru>
2020-09-28 23:58:26 +0300

    project 15 v1.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,8 +4,9 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   const err = new Error('Необходима авторизация');
   err.statusCode = 401;
-  if (!authorization && !authorization.startsWith('Bearer ')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     next(err);
+    return;
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
